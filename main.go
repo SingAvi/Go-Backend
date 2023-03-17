f3e0f3fac13b0af5d966352408cd4d2860c3ee9d
Avinash_Singh <desci.avinash@gmail.com>
2023-03-18 03:19:31 +0530

    main file

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"net/http"
+	"github.com/SingAvi/Go-Backend/controllers"
+)
 
 
 func main() {
 
-	fmt.Println("Hey SingAvi !")
+	router := httprouter.New()
+	uc := controllers.NewUserController(getSession())
 
+	router.GET("/user/:id", uc.GetUser)
+	router.POST("/user",uc.CreateUser)
+	r.DELETE("/user/:id",uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", router)
+
+}
+
+func getSession() *mgo.Session{
+
+	session, error := mgo.Dial("mongodb://localhost:27107")
+
+	if error != nil{
+		fmt.println("Error, session not started")
+	}
+	return session
 }
