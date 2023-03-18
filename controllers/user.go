bed3e973edde5ad74c0bc5279e779045d5587991
Avinash_Singh <desci.avinash@gmail.com>
2023-03-18 19:16:07 +0530

    updated controllers and models

--- controllers/user.go
+++ controllers/user.go
@@ -1,15 +1,17 @@
 package controllers
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/SingAvi/Go-Backend/models"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"net/http"
 )
 
-type User struct{
+type UserController struct{
 	session *mgo.Session
 }
 
@@ -17,7 +19,9 @@ func NewUserController(session *mgo.Session) *UserController{
 	return &UserController{session}
 }
 
-func (uc UserController) GetUser( writer http.ResponseWriter, request *http.Request, params httpRouter.Params){
+
+
+func (uc UserController) GetAllUser (writer http.ResponseWriter, request *http.Request, params httprouter.Params){
 
 	id := params.ByName("id")
 
@@ -25,10 +29,94 @@ func (uc UserController) GetUser( writer http.ResponseWriter, request *http.Requ
 		writer.WriteHeader(http.StatusNotFound)
 	}
 
-	oid := bson.ObjectHex(id)
 
 	user := models.User{}
 
-	uc.Session.DB("mongo-golang").C("users").FindId(oid)
+	if err := uc.session.DB("mongo-golang").C("users"); err!=nil{
+		writer.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(user)
+
+	if err!= nil{
+		fmt.Println(err)
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprint(writer,"%s\n",uj)
+
+}
+
+func (uc UserController) GetUser (writer http.ResponseWriter, request *http.Request, params httprouter.Params){
+
+	id := params.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		writer.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	user := models.User{}
+
+	if err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&user); err!=nil{
+		writer.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(user)
+
+	if err!= nil{
+		fmt.Println(err)
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprint(writer,"%s\n",uj)
+
+}
+
+
+func (uc UserController) CreateUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params){
+
+	user := models.User{}
+
+	json.NewDecoder(request.Body).Decode(&user)
+
+	user.Id = bson.NewObjectId()
+
+	uc.session.DB("mongo-golang").C("users").Insert(user)
+
+	uj,err := json.Marshal(user)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusCreated)
+	fmt.Println("User aDDED",uj)
+
+}
+
+func (uc UserController) DeleteUser(writer http.ResponseWriter, request *http.Request, params httprouter.Params){
+
+	id := params.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		writer.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	if err := uc.session.DB("mongo-golang").C("users").RemoveId(oid); err!=nil{
+		writer.WriteHeader(404)
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	fmt.Fprint(writer,"Deleted User",oid, "\n")
+
 }
 
--- models/user.go
+++ models/user.go
@@ -5,8 +5,8 @@ import (
 )
 
 type User struct{
-	Id          bson.ObjectId	`json:"id"` 			`bson:"_id"`
-	Name		string			`json:"name"` 			`bson:"name"`
-	Designation string			`json:"designation"` 	`bson:"designation"`
-	Experience	int				`json:"experience"` 	`bson:"experience"`
+	Id          bson.ObjectId	`json:"id"` 			
+	Name		string			`json:"name"` 			
+	Designation string			`json:"designation"` 
+	Experience	int				`json:"experience"` 	
 }
